745845dcb48b433606cb54c47fef9f77024979b2
Sowndarya19 <112232903+Sowndarya19@users.noreply.github.com>
2025-07-04 17:59:02 +0530

    Final version frontend and backend deployed successfully

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import sqlite3
+import os
 from datetime import datetime
 
 app = Flask(__name__)
@@ -59,4 +60,5 @@ def get_feedback():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
